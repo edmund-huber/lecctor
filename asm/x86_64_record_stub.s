c03dc0c1beebf4150c98b757e8d40dde0de93772
Edmund Huber <me@ehuber.info>
2020-10-16 21:01:20 -0700

    x86_64_record_stub.s is now actively recording trace blocks

--- asm/x86_64_record_stub.s
+++ asm/x86_64_record_stub.s
@@ -1,6 +1,6 @@
-	#cmpl $0, (%r15)
-	je NO_CALL_?NONCE?
-	call WAIT_FOR_TRACER
+	cmpl	$0, (%r15)
+	je	NO_CALL_?NONCE?
+	call	WAIT_FOR_TRACER
 NO_CALL_?NONCE?:
-	#movl ?TRACE_BLOCK_ID?, (%r15)
-	#add $4, %r15
+	movl	?TRACE_BLOCK_ID?, (%r15)
+	add	$4, %r15
