21406eb48311c7e20fe8393ccbfe22b7b6557479
Edmund Huber <me@ehuber.info>
2020-10-17 14:00:19 -0700

    use POSIX shm to store the tracer_struct, instead of a global variable (which was just a placeholder). This will allow us to write an as-attach program which maps in the same tracer_struct and reads out the data.
    Testing: ran ./gcc-tracer, afterwards there's a /dev/shm/as-tracer-$PID left
    over (because we explicitly are not unlinking the shm after the process dies).

--- c/x86_64_set_up_tracer.c
+++ c/x86_64_set_up_tracer.c
@@ -1,4 +1,10 @@
+#define _GNU_SOURCE
+#include <errno.h>
+#include <fcntl.h>
 #include <stdint.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
 #include "assert.h"
 
@@ -18,14 +24,31 @@ typedef struct tracer {
     uint8_t tracers_turn;
 } __attribute__((packed)) tracer_struct;
 
-tracer_struct should_be_shm;
-
 void set_up_tracer(void) {
     // If this changes, then all the asm code is wrong.
     ASSERT(sizeof(tracer_struct) == 138);
 
-    // TODO: shm initialization.
-    tracer_struct *trace = &should_be_shm;
+    // Set up a shared memory object:
+    // First, give it a name and create it ..
+    char shm_name[128] = { '\0' };
+    ASSERT(snprintf(shm_name, sizeof(shm_name), "/as-tracer-%i", getpid()) < sizeof(shm_name))
+    int fd;
+    if ((fd = shm_open(shm_name, O_RDWR | O_CREAT | O_EXCL, 0666)) == -1) {
+        printf("set_up_tracer: shm_open() failed with %s\n", strerror(errno));
+        return;
+    }
+    // .. size it properly ..
+    if (ftruncate(fd, sizeof(tracer_struct)) == -1) {
+        printf("set_up_tracer: ftruncate() failed with %s\n", strerror(errno));
+        return;
+    }
+    // .. and mmap it in.
+    tracer_struct *trace;
+    if ((trace = mmap(NULL, sizeof(tracer_struct), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0))
+        == MAP_FAILED) {
+        printf("set_up_tracer: mmap() failed with %s\n", strerror(errno));
+        return;
+    }
 
     // Initialize the tracer_struct.
     trace->magic = 0xbeefcafe;
