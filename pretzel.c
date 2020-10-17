aac5bad4dafbf479a05953e7fafa7408bbe27900
Edmund Huber <me@ehuber.info>
2020-10-16 20:55:47 -0700

    * "as-tracer-do-not-instrument" was a one-time use flag (per assembly file).   Instead, now instrumentation can be turned on and off as many times as you   like in one file with "as-tracer-do-instrument" and   "as-tracer-do-not-instrument". * "trust-me-i-know-what-im-doing" can now be used as many times as needed   in an assembly file. * pretzel.c more correctly sets up tracing: instrumentation needs to be disabled   until set_up_tracer() is called.

--- pretzel.c
+++ pretzel.c
@@ -1,11 +1,19 @@
 #include <stdlib.h>
 
+// "asm volatile" at file scope is broken in GCC 8.3:
+// https://gcc.gnu.org/bugzilla/show_bug.cgi?id=89585 .
+asm ( "\n# as-tracer-do-not-instrument" );
+
 void set_up_tracer(void);
 
 void pretzel(int);
 
 int main(int argc, char **argv) {
     set_up_tracer();
+
+    asm volatile (
+        "\n# as-tracer-do-instrument"
+    );
 
     if (argc != 2) {
         return 1;
